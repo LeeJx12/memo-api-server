afd8f092af010862eab90a83c083d1f94ae61f83
LeeJx2 <46637231+LeeJx12@users.noreply.github.com>
2022-09-20 18:39:12 +0900

    Feat: 초기세팅
    nestjs
    nestjs/config
    mongoose

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env'
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URL'),
+      }),
+      inject: [ConfigService],
+    })
+  ],
+  controllers: [AppController],
+  providers: [AppService, ConfigService],
+})
+export class AppModule {}
